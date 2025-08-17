ed9cedda1f4729fb40e1a4507bf832c046649084
Fábio <fabiomotamarques815@gmail.com>
2025-08-17 16:19:12 -0300

    Contador

--- app/src/main/java/br/com/ifsc/MainActivity.java
+++ app/src/main/java/br/com/ifsc/MainActivity.java
@@ -1,6 +1,9 @@
 package br.com.ifsc;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView contadorTextView;
+    private Button cliqueBotao;
+    private int contador = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        contadorTextView = findViewById(R.id.contadorTextView);
+        cliqueBotao = findViewById(R.id.cliqueBotao);
+
+        cliqueBotao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contador++;
+                atualizarContador();
+            }
         });
+    }
+
+    private void atualizarContador() {
+        contadorTextView.setText(String.valueOf(contador));
     }
 }
