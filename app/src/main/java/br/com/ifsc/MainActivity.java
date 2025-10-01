dba1351666d64e8b900853c4cfe9cc0ebee6b641
fabiomotamarques815@gmail.com <fabiomotamarques815@gmail.com>
2025-10-01 19:44:32 -0300

    enviando dados para a segunda tela

--- app/src/main/java/br/com/ifsc/MainActivity.java
+++ app/src/main/java/br/com/ifsc/MainActivity.java
@@ -1,6 +1,9 @@
 package br.com.ifsc;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText altura;
+    EditText peso;
+    EditText nome;
+    Button btnCalcular;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        altura = findViewById(R.id.altura);
+        peso = findViewById(R.id.peso);
+        btnCalcular = findViewById(R.id.btnCalcular);
+        nome = findViewById(R.id.nome);
+
+        btnCalcular.setOnClickListener(v -> {
+            Intent intent = new Intent(this, SegundActivity.class);
+            Float p = Float.parseFloat(peso.getText().toString());
+            Float a = Float.parseFloat(altura.getText().toString());
+            intent.putExtra("peso", p);
+            intent.putExtra("altura", a);
+            intent.putExtra("nome", nome);
+            startActivity(intent);
         });
     }
 }
