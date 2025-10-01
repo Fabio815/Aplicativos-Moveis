5cb8e98a0c275603f82754d731ef41baf42ac671
fabiomotamarques815@gmail.com <fabiomotamarques815@gmail.com>
2025-10-01 20:47:40 -0300

    falta odicionar o fragment

--- app/src/main/java/br/com/ifsc/MainActivity2.java
+++ app/src/main/java/br/com/ifsc/MainActivity2.java
@@ -2,6 +2,7 @@ package br.com.ifsc;
 
 import android.media.Image;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +17,8 @@ public class MainActivity2 extends AppCompatActivity {
     TextView nome;
     TextView imcCalculado;
     ImageView img;
+    Button btnVoltar;
+    TextView ds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,8 @@ public class MainActivity2 extends AppCompatActivity {
         nome = findViewById(R.id.nome);
         imcCalculado = findViewById(R.id.imcCalculado);
         img = findViewById(R.id.imagem);
+        btnVoltar = findViewById(R.id.voltar);
+        ds = findViewById(R.id.ds);
 
         Bundle bundle = getIntent().getExtras();
 
@@ -35,11 +40,38 @@ public class MainActivity2 extends AppCompatActivity {
         String nomeTexto = bundle.getString("nome");
 
         double imc = peso / Math.pow(altura, 2);
-        nome.setText("Nome: " + String.valueOf(nome).toString());
-        imcCalculado.setText("IMC: " + String.valueOf(Math.round(imc)));
-        /*btnVoltar.findViewById(R.id.voltar);
+        nome.setText("Nome: " + nomeTexto);
+        /*imcCalculado.setText("IMC: " + String.valueOf(Math.round(imc)));*/
+
+        if (imc < 18.5) {
+            imcCalculado.setText("Abaixo do peso: " + Math.round(imc));
+            img.setImageResource(R.drawable.abaixopeso);
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            imcCalculado.setText("Peso ideal: " + Math.round(imc));
+            img.setImageResource(R.drawable.normal);
+        } else if (imc > 24.9 && imc <= 29.9) {
+            imcCalculado.setText("Sobrepeso: " + Math.round(imc));
+            img.setImageResource(R.drawable.sobrepeso);
+        } else if (imc > 30 && imc <= 34.9) {
+            imcCalculado.setText("Obesidade 1: " + Math.round(imc));
+            img.setImageResource(R.drawable.obesidade1);
+        } else if (imc > 35 && imc <= 35) {
+            imcCalculado.setText("Obesidade 2: " + Math.round(imc));
+            img.setImageResource(R.drawable.obesidade2);
+        } else {
+            imcCalculado.setText("Obesidade 3: " + Math.round(imc));
+            img.setImageResource(R.drawable.obesidade3);
+        }
+
+        if (imc < 20) {
+            ds.setText("Precisa ganhar peso");
+        } else if (imc > 25) {
+            ds.setText("Precisa ganhar perder");
+        }
+
+        btnVoltar.findViewById(R.id.voltar);
         btnVoltar.setOnClickListener(v -> {
             finish();
-        });*/
+        });
     }
 }
