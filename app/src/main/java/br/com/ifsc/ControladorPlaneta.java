da3a5affec64c2802273d8967cd614d4522d2ea7
Fabio <fabiomotamarques815@gmail.com>
2025-10-08 21:31:04 -0300

    criado as classes

---
+++ app/src/main/java/br/com/ifsc/ControladorPlaneta.java
@@ -0,0 +1,16 @@
+package br.com.ifsc;
+
+public class ControladorPlaneta {
+    DAOPlaneta dao;
+    public ControladorPlaneta() {
+        dao = new DAOPlaneta();
+        dao.inserirPlanetas(new Planeta("Mercurio", R.drawable.mercury));
+        dao.inserirPlanetas(new Planeta("Venus", R.drawable.venus));
+        dao.inserirPlanetas(new Planeta("Terra", R.drawable.earth));
+        dao.inserirPlanetas(new Planeta("Jupiter", R.drawable.jupter));
+        dao.inserirPlanetas(new Planeta("Saturno", R.drawable.saturn));
+        dao.inserirPlanetas(new Planeta("Urano", R.drawable.uranus));
+        dao.inserirPlanetas(new Planeta("Netuno", R.drawable.neptune));
+
+    }
+}
