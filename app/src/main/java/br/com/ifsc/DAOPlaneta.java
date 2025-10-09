7ee4bff2c27618b7a1284d1e968e398539212726
Fabio <fabiomotamarques815@gmail.com>
2025-10-08 21:24:27 -0300

    mudando o listView

--- app/src/main/java/br/com/ifsc/DAOPlaneta.java
+++ app/src/main/java/br/com/ifsc/DAOPlaneta.java
@@ -1,5 +1,24 @@
 package br.com.ifsc;
 
-public class DAOPlaneta {
+import java.util.ArrayList;
 
+public class DAOPlaneta {
+    private ArrayList<Planeta> planetas;
+
+    public DAOPlaneta() {
+        planetas = new ArrayList<Planeta>();
+    }
+
+    //Inserir Planetas
+    public void inserirPlanetas(Planeta p) {
+        this.planetas.add(p);
+    }
+
+    public ArrayList<Planeta> getPlanetas() {
+        return planetas;
+    }
+
+    public Planeta get(int posicao) {
+        return  this.planetas.get(posicao);
+    }
 }
