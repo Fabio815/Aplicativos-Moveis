7a9c33eedbe3f1cb1814a35ece386d3d25a27f73
unknown <fabiomotamarques815@gmail.com>
2025-10-22 21:48:00 -0300

    coloquei o pickerColor

--- app/src/main/java/br/com/ifsc/MainActivity.java
+++ app/src/main/java/br/com/ifsc/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.ifsc;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.skydoves.colorpickerview.ColorEnvelope;
+import com.skydoves.colorpickerview.ColorPickerDialog;
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView contadorTextView;
@@ -21,9 +26,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(v -> {
+            new ColorPickerDialog.Builder(this)
+                    .setTitle("ColorPicker Dialog")
+                    .setPreferenceName("MyColorPickerDialog")
+                    .setPositiveButton("Confirmar",
+                            new ColorEnvelopeListener() {
+                                @Override
+                                public void onColorSelected(ColorEnvelope envelope, boolean fromUser) {
+                                    setColor(envelope);
+                                }
+                            })
+                    .setNegativeButton("Cancelar",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            })
+                    .attachAlphaSlideBar(true) // the default value is true.
+                    .attachBrightnessSlideBar(true)  // the default value is true.
+                    .setBottomSpace(12) // set a bottom space between the last slidebar and buttons.
+                    .show();
+        });
+
+
     }
 
-    private void atualizarContador() {
-        contadorTextView.setText(String.valueOf(contador));
+    public void setColor(ColorEnvelope envelope) {
+
     }
+
 }
