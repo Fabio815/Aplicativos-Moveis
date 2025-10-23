80aa94b2fe6ae783af7c3c88d37f28033b4922a6
unknown <fabiomotamarques815@gmail.com>
2025-10-22 21:01:07 -0300

    Muda a cor do botão conforme o mause

--- app/src/main/java/br/com/ifsc/TinderButton.java
+++ app/src/main/java/br/com/ifsc/TinderButton.java
@@ -2,6 +2,7 @@ package br.com.ifsc;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -13,7 +14,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class TinderButton extends androidx.appcompat.widget.AppCompatButton {
-
+    //Em relação ao botão.
+    int x0;
+    int y0;
+    float colorR;
+    float colorG;
+    float colorB;
     public TinderButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         this.setText("Tinder Button");
@@ -36,10 +42,26 @@ public class TinderButton extends androidx.appcompat.widget.AppCompatButton {
     //Trata o evento de clique, a skia procura qual o componente que vai ser procurado.
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        float x = event.getX();
-        float y = event.getY();
+        //Em relação ao botão.
+        float x = event.getRawX();
+        float y = event.getRawY();
 
-        Log.d("Posição", "X: " + x + "Posição Y: " + y);
+        Log.d("Posição", "X: " + x + " Posição Y: " + y + " Event: " + event.getAction());
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            x0 = (int)x;
+            y0 = (int)y;
+        }
+        int dx = (int)(x - x0);
+        if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            colorR = Math.min(255, Math.max(0, 120 - dx / 5));
+            colorG = Math.min(255, Math.max(0, 120 + dx / 5));
+            colorB = 120;
+        }
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            colorR = 120; colorG = 120; colorB = 120;
+        }
+        this.setBackgroundColor(Color.rgb(colorR, colorG, colorB));
+
         return super.onTouchEvent(event);
     }
 }
