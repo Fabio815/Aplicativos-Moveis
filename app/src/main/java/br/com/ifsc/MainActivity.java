382f50006a87e1dd628e21afc7c40bcbc53c170d
unknown <fabiomotamarques815@gmail.com>
2025-11-05 20:42:51 -0300

    sqlite

--- app/src/main/java/br/com/ifsc/MainActivity.java
+++ app/src/main/java/br/com/ifsc/MainActivity.java
@@ -1,8 +1,15 @@
 package br.com.ifsc;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,30 +18,51 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    SQLiteDatabase database;
+    EditText editText;
+    Button botao;
+    ListView listView;
 
-    private TextView contadorTextView;
-    private Button cliqueBotao;
-    private int contador = 0;
-
+    ArrayList<String> notasList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText  = findViewById(R.id.nome);
+        botao = findViewById(R.id.button);
+        listView = findViewById(R.id.listView);
+        database = openOrCreateDatabase("app_database", MODE_PRIVATE, null);
 
-        contadorTextView = findViewById(R.id.contadorTextView);
-        cliqueBotao = findViewById(R.id.cliqueBotao);
-
-        cliqueBotao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                contador++;
-                atualizarContador();
+        database.execSQL("create table if not exists notas (id integer primary key autoincrement, name varchar, texto varchar)");
+        botao.setOnClickListener( v -> {
+            String texto = editText.getText().toString();
+            if (!texto.isEmpty()) {
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("name",  texto);
+                contentValues.put("texto", texto);
+                database.insert("notas", null, contentValues);
             }
+
         });
+
     }
 
-    private void atualizarContador() {
-        contadorTextView.setText(String.valueOf(contador));
+    public void carregarNotas() {
+        //database.rawQuery("select * from notas where id=?", new String[]{"1"});
+        notasList.clear();
+        Cursor cursor = database.rawQuery("select * from notas", null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            int columnIndex = cursor.getColumnIndex("texto");
+            String name = cursor.getString(columnIndex);
+            notasList.add(name);
+            cursor.moveToNext();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, notasList);
+        listView.setAdapter(adapter);
     }
 }
