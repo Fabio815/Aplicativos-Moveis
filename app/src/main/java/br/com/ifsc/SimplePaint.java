cf255143051f43a29e2af3631b58fdef74dbfa99
Fabio <fabiomotamarques815@gmail.com>
2025-11-05 17:20:12 -0300

    dando erro no botão

--- app/src/main/java/br/com/ifsc/SimplePaint.java
+++ app/src/main/java/br/com/ifsc/SimplePaint.java
@@ -5,34 +5,48 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.drawable.ColorDrawable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SimplePaint extends View {
     List<Paint> mPaintList;
     List<Path> mPathList;
-    private final Paint mPaint;
-    private final Path mPath;
+    private Paint mPaint;
+    private Path mPath;
+    ColorDrawable currentColor;
 
     public SimplePaint(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mPaintList = new ArrayList<Paint>();
+        mPathList = new ArrayList<Path>();
+        mPaint.setColor(Color.BLACK);
+        currentColor = new ColorDrawable();
+        currentColor.setColor(Color.BLACK);
+        initLayerDraq();
+    }
+
+    public void initLayerDraq() {
         mPaint = new Paint();
         mPath = new Path();
 
-        mPaint.setColor(Color.RED);
         mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeWidth(8f);
-        mPaint.setAntiAlias(true);
+        mPaint.setStrokeWidth(20);
+        mPaint.setColor(currentColor.getColor());
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        for (int i = 0; i < mPathList.size(); i++) {
+            canvas.drawPath(mPathList.get(i), mPaintList.get(i));
+        }
         canvas.drawPath(mPath, mPaint);
     }
 
@@ -44,10 +58,18 @@ public class SimplePaint extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 mPath.moveTo(lx, ly);
-                return true;
+                mPath.lineTo(lx, ly);
+                break;
             case MotionEvent.ACTION_MOVE:
+                mPath.lineTo(lx, ly);
+                break;
             case MotionEvent.ACTION_UP:
                 mPath.lineTo(lx, ly);
+                mPaintList.add(mPaint);
+                mPathList.add(mPath);
+                initLayerDraq();
+                break;
+            default:
                 break;
         }
 
@@ -56,6 +78,7 @@ public class SimplePaint extends View {
     }
 
     public void setcColor(Color color) {
+        currentColor.setColor(color.toArgb());
         mPaint.setColor(color.toArgb());
     }
 }
