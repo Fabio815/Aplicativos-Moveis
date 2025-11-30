629c20c690d4fc5384f7a9f32ac2ea3a68d7a998
Fabio <fabiomotamarques815@gmail.com>
2025-11-30 12:36:50 -0300

    Falta funcionar o mapinha :(

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     implementation("org.osmdroid:osmdroid-android:6.1.16")
     androidTestImplementation(libs.ext.junit)
--- app/src/main/java/br/com/ifsc/MainActivity.java
+++ app/src/main/java/br/com/ifsc/MainActivity.java
@@ -1,7 +1,5 @@
 package br.com.ifsc;
 
-import static androidx.appcompat.app.AppCompatActivity.*;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
@@ -9,78 +7,70 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.Manifest;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.RequiresPermission;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
 public class MainActivity extends AppCompatActivity {
+
     LocationManager locationManager;
     TextView textView;
     Button btn;
-    MapView mapView
+    MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        //setContentView(R.layout.activity_main4);
+
+        setContentView(R.layout.activity_main);
+
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
         textView = findViewById(R.id.textView);
         btn = findViewById(R.id.button);
         mapView = findViewById(R.id.mapinha);
 
-        btn.setOnClickListener( v -> {
-            getLocation();
-        });
+        btn.setOnClickListener(v -> getLocation());
     }
 
     @SuppressLint("MissingPermission")
-    public void getLocation(){
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, location1 -> {
-            double latitude = location.getLatitude();
-            double longitude = location.getLongitude();
-            textView.setText("longitude: " + latitude + " Longitude: " + longitude);
-        });
-        if (ActivityCompat.checkSelfPermission(
-                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED)
+    public void getLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
             requestPermission();
             return;
         }
 
-        if(location != null){
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if (location != null) {
             double latitude = location.getLatitude();
             double longitude = location.getLongitude();
-            textView.setText("longitude: " + latitude + " Longitude: " + longitude);
+
+            textView.setText("Latitude: " + latitude + " Longitude: " + longitude);
+
             showLocationOnMap(latitude, longitude);
         }
-
     }
-    public void showLocationOnMap(double latitude, double longitude ) {
-        GeoPoint userLocation = new GeoPoint(latitude, longitude);
 
-        mapView.getController(userLocation);
+    public void showLocationOnMap(double latitude, double longitude ) {
+        GeoPoint point = new GeoPoint(latitude, longitude);
+
+        mapView.getController().setCenter(point);
+        mapView.getController().setZoom(18);
+        mapView.invalidate();
     }
 
     public void requestPermission(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
-            requestPermissions(
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1
-            );
-        }
+        ActivityCompat.requestPermissions(
+                this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1
+        );
     }
 }
